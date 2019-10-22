614a0e1739974b869311d54635a88eeeb06d4e3e
Mindaugas Kuciauskas (MKUC) <mkuc@oticon.com>
2019-10-22 12:55:05 +0200

    working version (locally)

--- AddDetailedGame.cs
+++ AddDetailedGame.cs
@@ -1,26 +1,29 @@
-
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 namespace bgg
 {
-    public static class cloneBggToLocalTable
+    public static class AddDetailedGame
     {
-        [FunctionName("cloneBggToLocalTable")]
+        [FunctionName("AddDetailedGame")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] GameInfo game,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             [Table("Games")] IAsyncCollector<GameInfo> gamesInfoTable,
             ILogger log)
         {
 
-            game.PartitionKey = "games";
-            game.RowKey = game.gameTitle;
-            await gamesInfoTable.AddAsync(game);
+            var gameId = req.Query["gameId"];
+            var result = await GetGameInfoLogic.GetGameDetails(gameId);
 
-            var serializeObject = Newtonsoft.Json.JsonConvert.SerializeObject(game);
+            result.PartitionKey = "games";
+            result.RowKey = result.gameTitle;
+            await gamesInfoTable.AddAsync(result);
+
+            var serializeObject = Newtonsoft.Json.JsonConvert.SerializeObject(result);
 
             return serializeObject != null
                 ? (ActionResult)new OkObjectResult($"Added: {serializeObject}")
