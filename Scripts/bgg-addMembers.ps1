5a69ab6cfd8b2d64041eafd6daf3ca7508335bb3
Mindaugas Kuciauskas (MKUC) <mkuc@oticon.com>
2020-07-09 09:10:41 +0200

    add checkAccess

--- Scripts/bgg-addMembers.ps1
+++ Scripts/bgg-addMembers.ps1
@@ -1,9 +1,12 @@
 $members = 'KEAN', 'LTBE', 'JACB', 'RCHE', 'DQEL', 'OLEO', 'ACGO', 'RUGR', 'KAHD', 'AJHA', 'THOH', 'ENHE', 'JOIS', 'CHJA', 'SIJH', 'MKUC', 'GUSL', 'THMO', 'CNEL', 'SEOA', 'YUPO', 'ACRU', 'MRYT', 'PLSO', 'PSZY', 'KIRH', 'TEWO', 'DNHN';
 
-$baseUrl = 'http://localhost:7071/api';
-# $baseUrl = 'https://bgg-api.azurewebsites.net/api';
+# $baseUrl = 'http://localhost:7071/api';
+$baseUrl = 'https://bgg-api.azurewebsites.net/api';
 #add games
 foreach ($initials in $members) {
+    $urlToInvoke1 = $baseUrl + '/DeleteMember?initials=' + $initials
+    Write-Host $urlToInvoke1
+    Invoke-RestMethod $urlToInvoke1
     $urlToInvoke = $baseUrl + '/AddMember?initials=' + $initials
     Write-Host $urlToInvoke
     Invoke-RestMethod $urlToInvoke
---
+++ members/CheckAccess.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace bgg
+{
+    public static class CheckAccess
+    {
+        [FunctionName("CheckAccess")]
+        public static async Task<ActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            // expecting comma seperated initials
+            var initials = req.Query["initials"];
+            var pass = req.Query["pass"];
+
+            if (string.IsNullOrEmpty(initials) || string.IsNullOrEmpty(pass))
+            {
+                return new BadRequestObjectResult($"FAILED!");
+            }
+
+            return pass == "2020 Oticon board games club!!!" ? (ActionResult)new OkObjectResult("FINE.") : new BadRequestObjectResult($"FAILED!");
+        }
+    }
+}
