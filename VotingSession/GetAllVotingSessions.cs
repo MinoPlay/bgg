98cd5b335319e95f22937d533fac13d0f8297ebf
Mindaugas Kuciauskas (MKUC) <mkuc@oticon.com>
2020-07-16 14:33:09 +0200

    getAllVotingSessions takes optional latest flag

--- VotingSession/GetAllVotingSessions.cs
+++ VotingSession/GetAllVotingSessions.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -18,6 +19,13 @@ namespace bgg
         {
             var query = new TableQuery<VotingSession>();
             var result = await votingSessionsTable.ExecuteQuerySegmentedAsync(query, null);
+
+            var sessionId = req.Query["sessionId"];
+            if (!string.IsNullOrEmpty(sessionId) && sessionId == "active")
+            {
+                var active = result.Results.Where(x => x.Active);
+                return new JsonResult(active);
+            }
 
             return new JsonResult(result.Results);
         }
