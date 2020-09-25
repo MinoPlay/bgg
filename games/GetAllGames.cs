d49a8d4040f796063e8c98d504aebc6943e2f06f
Mindaugas Kuciauskas (MKUC) <mkuc@oticon.com>
2020-09-25 12:08:05 +0200

    two new functions GetAllGames and GetGamesStates

---
+++ games/GetAllGames.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace bgg
+{
+    public static class GetAllGames
+    {
+        [FunctionName("GetAllGames")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [Table("Games", "games")] CloudTable games,
+            ILogger log)
+        {
+            var query = new TableQuery<GameInfo>();
+            var segment = await games.ExecuteQuerySegmentedAsync(query, null);
+
+            return new JsonResult(segment.Results);
+        }
+    }
+}
