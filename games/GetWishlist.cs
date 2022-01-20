6b8163deee184348d6bab3d45aa24485f4407ea6
Mindaugas Kuciauskas (MKUC) <mkuc@oticon.com>
2022-01-20 14:23:54 +0100

    add option to query wishlist based on session

--- games/GetWishlist.cs
+++ games/GetWishlist.cs
@@ -5,6 +5,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Cosmos.Table;
+using System.Linq;
 
 namespace bgg
 {
@@ -14,6 +15,7 @@ namespace bgg
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             [Table("Games", "games")] CloudTable games,
+            [Table("VotingSessionEntries", "votingSessionEntry")] CloudTable votingSessionEntriesTable,
             ILogger log)
         {
             var query = new TableQuery<GameInfo>();
@@ -22,7 +24,14 @@ namespace bgg
             var sessionId = req.Query["sessionId"];
             if (!string.IsNullOrEmpty(sessionId))
             {
+                var query2 = new TableQuery<VotingSessionEntry>();
+                var retrievedSessionEntries = await votingSessionEntriesTable.ExecuteQuerySegmentedAsync(query2, null);
 
+                log.LogInformation($"Trying to retrieve games for session: '{sessionId}'");
+                var sessionEntries = retrievedSessionEntries.Results.Where(x => x.VotingSessionId == sessionId);
+                var result = segment.Results.Where(x => sessionEntries.Any(y => y.GameId == x.GameId));
+
+                return new JsonResult(result);
             }
 
             return new JsonResult(segment.Results);
